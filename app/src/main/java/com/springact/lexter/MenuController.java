5c10e4aff2feff9d86755bee165ab93e6f742d3a
Lexter Tigas <tigaslexter@yahoo.com>
2019-10-31 13:29:37 +0800

    Removed no index method

--- app/src/main/java/com/springact/lexter/MenuController.java
+++ app/src/main/java/com/springact/lexter/MenuController.java
@@ -2,13 +2,29 @@ package com.spring.lexter;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import java.util.List;
+
 
 @Controller
 public class MenuController {
 
-	@RequestMapping("/")
-	public String index() {
+	// @RequestMapping("/index")
+	// public String index() {
+	// 	return "index";
+	// }
+
+
+	@RequestMapping(value="/index")
+	public String roleViewPage(ModelMap model, @RequestParam String choice) {
+		System.out.println(choice);
+		if(choice.equalsIgnoreCase("role")) {
+			model.put("choice",choice);
+			return "rolesview";
+		}
 		return "index";
 	}
 }
