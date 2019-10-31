d2d3d2460e476426b5b1832770794a3bfa34cbe4
Lexter Tigas <tigaslexter@yahoo.com>
2019-10-31 08:23:16 +0800

    Disabled springn security

--- app/src/main/java/com/springact/lexter/SecurityConfiguration.java
+++ app/src/main/java/com/springact/lexter/SecurityConfiguration.java
@@ -1,12 +1,12 @@
-package com.spring.lexter;
+// package com.spring.lexter;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-@Configuration
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.authorizeRequests().antMatchers("/").permitAll();
-    }
-}
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+// import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+// @Configuration
+// public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+//     @Override
+//     protected void configure(HttpSecurity httpSecurity) throws Exception {
+//         httpSecurity.authorizeRequests().antMatchers("/").permitAll();
+//     }
+// }
